15c546bf7f0ffc27e7d2e55c7386e6a72cf732c2
zeniian <zeniian9@gmail.com>
2022-10-11 09:46:39 +0200

    add comment

--- src/it/ian/scuola/cli/oop/persona/Main.java
+++ src/it/ian/scuola/cli/oop/persona/Main.java
@@ -2,12 +2,19 @@ package it.ian.scuola.cli.oop.persona;
 
 public class Main {
     public static void main(String[] args) {
+       //dichiarazione degli oggetti
         Persona persona1 = new Persona("Marco", "Bianchi");
         Persona persona2 = new Persona("Renato", "Caputo", "Sig.", "barbiere");
+
+        //stampre a schermo le informazioni delle due persone
         System.out.println(persona1.ottieniInfoPersona());
         System.out.println(persona2.ottieniInfoPersona());
+
+        //cambio dei parametri dell oggetto persona 1
         persona1.setTitolo("Ing.");
         persona1.setProfessione("analista programmatore");
+
+        //stampare a schermo l oggetto persona 1 modificato
         System.out.println(persona1.ottieniInfoPersona());
     }
 }
